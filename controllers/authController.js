54ecc45d2fac139c076e719e4792b837835be2ef
ankitmaharshi29 <ankitmaharshi55@gmail.com>
2024-08-05 23:33:22 +0530

    Initial commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const {generateToken }= require("../utils/generateToken");
 const userModel = require("../models/user-model");
 const bcrypt = require("bcrypt");
-//const jwt= require("jsonwebtoken")
+const jwt= require("jsonwebtoken")
 
 module.exports.registerUser =  (req, res) => {
   try {
