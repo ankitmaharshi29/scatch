adf72f00bbedeaa231bd1c516bc3fcaa699f0fc9
ankitmaharshi29 <ankitmaharshi55@gmail.com>
2024-08-06 00:05:17 +0530

    check

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const {generateToken }= require("../utils/generateToken");
+//const {generateToken }= require("../utils/generateToken");
 const userModel = require("../models/user-model");
 const bcrypt = require("bcrypt");
 const jwt= require("jsonwebtoken")
